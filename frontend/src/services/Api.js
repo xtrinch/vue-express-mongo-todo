7e3dc90f0410d1fe5a615d95f5ad4c32d285a7c1
xtrinch <mojca.rojko@gmail.com>
2018-08-29 14:59:53 +0200

    Fix wrong API call location

--- frontend/src/services/Api.js
+++ frontend/src/services/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default() => {
   return axios.create({
-    baseURL: `http://${window.location.hostname}:${process.env.PORT || 8081}`
+    baseURL: `http://${window.location.hostname}:${process.env.NODE_ENV=='production' ? 80 : 8081}`
   })
 }
